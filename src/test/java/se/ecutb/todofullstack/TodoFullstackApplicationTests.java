2fb45b9d1912586aba7ae2258f6eedf47fa74468
Loffe Knutsson <loffe.knutsson@gmail.com>
2020-03-30 13:15:35 +0200

    first commit

---
+++ src/test/java/se/ecutb/todofullstack/TodoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ecutb.todofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoFullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
